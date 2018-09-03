cc5a536815bc264110664e5bcc5c4a76f3fc6283
Philip Mathew <philip.mathew@hcl.com>
2018-09-03 16:58:09 +0530

    Second Commit

---
+++ core/src/main/java/com/aem/community/core/models/StockListing.java
@@ -0,0 +1,30 @@
+package com.aem.community.core.models;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+
+import java.util.*;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+@Model(adaptables=Resource.class)
+public class StockListing {
+	 private List<String> list;
+	 
+	 private String message;
+
+	    @PostConstruct
+	    protected void init() {
+	    	list= new ArrayList<>();
+			 list.add("A");
+			 list.add("V");
+	        message = "\tHello World!\n";
+	    }
+	    
+	public List<String> getList(){		
+		return list;
+	}
+	 
+
+}
