2bd8b6017b8db951cb587403f7732e715706624a
Philip Mathew <philip.mathew@hcl.com>
2018-09-05 16:54:52 +0530

    05Sepcommit

--- core/src/main/java/com/aem/community/core/models/StockListing.java
+++ core/src/main/java/com/aem/community/core/models/StockListing.java
@@ -17,7 +17,7 @@ public class StockListing {
 	    @PostConstruct
 	    protected void init() {
 	    	list= new ArrayList<>();
-			 list.add("APHILIP Test");
+			 list.add("APHILIP Test Afeter lunch");
 			 list.add("VSANTHOSH Second");
 	        message = "\tHello World !\n";
 	    }
